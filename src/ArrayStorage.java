82c4103a30703fab2875800f154b3973d9cf67ef
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-02-24 15:25:03 +0300

    Updated_ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,62 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+	private Resume[] storage = new Resume[10000];
+	private int size = 0;
 
-    void clear() {
-    }
+	void clear() {
+		Arrays.fill(storage, null);
+		size = 0;
+	}
 
-    void save(Resume r) {
-    }
+	void save(Resume input) {
+		for (Resume resume : storage) {
+			if ((resume != null) && (input.toString().equals(resume.toString()))) {
+				System.out.println("Неуникальный(повторяющийся) uuid");
+				return;
+			}
+		}
+		storage[size] = input;
+		size++;
+	}
 
-    Resume get(String uuid) {
-        return null;
-    }
+	Resume get(String uuid) {
+		for (Resume resume : storage) {
+			if ((resume != null) && (resume.toString().equals(uuid))) {
+				return resume;
+			}
+		}
+		return null;
+	}
 
-    void delete(String uuid) {
-    }
+	void delete(String uuid) {
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
-    }
+		for (int i = 0; i < size; i++) {
+			if (storage[i].uuid.equals(uuid)) {
+				size--;
+				if (size - i >= 0) {
+					System.arraycopy(storage, i + 1, storage, i, size - i);
+				}
+			}
 
-    int size() {
-        return 0;
-    }
+		}
+	}
+
+	/**
+	 * @return array, contains only Resumes in storage (without null)
+	 */
+	Resume[] getAll() {
+
+		Resume[] notnull = new Resume[size];
+		System.arraycopy(storage, 0, notnull, 0, size);
+
+		return notnull;
+	}
+
+	int size() {
+		return size;
+	}
 }
