bc7df790940f35eaa2037957148ce9990359d7b6
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-02-25 23:56:04 +0300

    Fix ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,62 +1,63 @@
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-	private Resume[] storage = new Resume[10000];
-	private int size = 0;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-	void clear() {
-		Arrays.fill(storage, null);
-		size = 0;
-	}
+    void clear() {
+        if (size > 0) {
+            for (int i = 0; i < size; i++) {
+                storage[i] = null;
+            }
+            size = 0;
+        }
+    }
 
-	void save(Resume input) {
-		for (Resume resume : storage) {
-			if ((resume != null) && (input.toString().equals(resume.toString()))) {
-				System.out.println("Неуникальный(повторяющийся) uuid");
-				return;
-			}
-		}
-		storage[size] = input;
-		size++;
-	}
+    void save(Resume input) {
+        for (int i = 0; i < size; i++) {
+            Resume resume = storage[i];
+            if (input.toString().equals(resume.toString())) {
+                System.out.println("Неуникальный(повторяющийся) uuid");
+                return;
+            }
+        }
+        storage[size] = input;
+        size++;
+    }
 
-	Resume get(String uuid) {
-		for (Resume resume : storage) {
-			if ((resume != null) && (resume.toString().equals(uuid))) {
-				return resume;
-			}
-		}
-		return null;
-	}
+    Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            Resume resume = storage[i];
+            if (resume.toString().equals(uuid)) {
+                return resume;
+            }
+        }
+        return null;
+    }
 
-	void delete(String uuid) {
+    void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            Resume resume = storage[i];
+            if (resume.toString().equals(uuid)) {
+                size--;
+                if (size - i >= 0) {
+                    System.arraycopy(storage, i + 1, storage, i, size - i);
+                }
+            }
+        }
+    }
 
-		for (int i = 0; i < size; i++) {
-			if (storage[i].uuid.equals(uuid)) {
-				size--;
-				if (size - i >= 0) {
-					System.arraycopy(storage, i + 1, storage, i, size - i);
-				}
-			}
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        Resume[] resumes = new Resume[size];
+        System.arraycopy(storage, 0, resumes, 0, size);
+        return resumes;
+    }
 
-		}
-	}
-
-	/**
-	 * @return array, contains only Resumes in storage (without null)
-	 */
-	Resume[] getAll() {
-
-		Resume[] notnull = new Resume[size];
-		System.arraycopy(storage, 0, notnull, 0, size);
-
-		return notnull;
-	}
-
-	int size() {
-		return size;
-	}
+    int size() {
+        return size;
+    }
 }
