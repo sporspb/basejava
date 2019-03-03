ec654576abfab7b595f3103077657756a7a1fe17
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-03-03 16:07:51 +0300

    HW2 Fix ArrayStorage.java, MainArray.java
    Renamed getMatch method to 'getIndex' in ArrayStorage.java
    Fixed get and delete methods in ArrayStorage.java
    Added text to MainArray.java

--- src/com/spor/webapp/storage/ArrayStorage.java
+++ src/com/spor/webapp/storage/ArrayStorage.java
@@ -6,7 +6,7 @@ import com.spor.webapp.model.Resume;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private final int STORAGE_SIZE = 10000;
+    private final int STORAGE_SIZE = 10_000;
     private Resume[] storage = new Resume[STORAGE_SIZE];
     private int size = 0;
 
@@ -18,7 +18,7 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
-        int index = getMatch(resume.getUuid());
+        int index = getIndex(resume.getUuid());
 
         if (size == STORAGE_SIZE) {
             System.out.println("Not enough space in storage");
@@ -31,7 +31,7 @@ public class ArrayStorage {
     }
 
     public void update(Resume resume) {
-        int index = getMatch(resume.getUuid());
+        int index = getIndex(resume.getUuid());
 
         if (index == -1) {
             System.out.println("Resume not found");
@@ -41,36 +41,26 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-        int index = getMatch(uuid);
+        int index = getIndex(uuid);
 
         if (index == -1) {
             System.out.println("Resume not found");
+            return null;
         } else {
             return storage[index];
         }
-        return null;
     }
 
     public void delete(String uuid) {
-        int index = getMatch(uuid);
+        int index = getIndex(uuid);
 
         if (index == -1) {
             System.out.println("Resume not found");
         } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
             size--;
-            if (size - index >= 0) {
-                System.arraycopy(storage, index + 1, storage, index, size - index);
-            }
         }
-    }
-
-    private int getMatch(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return i;
-            }
-        }
-        return -1;
     }
 
     /**
@@ -84,5 +74,14 @@ public class ArrayStorage {
 
     public int size() {
         return size;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
