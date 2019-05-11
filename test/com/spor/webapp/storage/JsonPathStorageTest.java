af83c47d56d93cd7505cfe74224a6cd9d9816ecf
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-05-11 21:29:40 +0300

    HW10 add JSON
    Added JsonSectionAdapter.java
    Added JsonStreamSerializer.java
    Added JsonParserParser.java
    Added JsonPathStorageTest.java

---
+++ test/com/spor/webapp/storage/JsonPathStorageTest.java
@@ -0,0 +1,9 @@
+package com.spor.webapp.storage;
+
+import com.spor.webapp.storage.Serializer.JsonStreamSerializer;
+
+public class JsonPathStorageTest extends AbstractStorageTest {
+    public JsonPathStorageTest() {
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new JsonStreamSerializer()));
+    }
+}
