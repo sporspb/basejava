cc562c14a7910cd892f855fdd0ea40bbf794e139
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-05-14 05:37:23 +0300

    HW11 add LazySingleton

---
+++ src/com/spor/webapp/LazySingleton.java
@@ -0,0 +1,24 @@
+package com.spor.webapp;
+
+public class LazySingleton {
+    volatile private static LazySingleton INSTANCE;
+
+    private LazySingleton() {
+    }
+
+    public static LazySingleton getInstance() {
+        return LazySingletonHolder.INSTANCE;
+//        if (INSTANCE == null) {
+//            synchronized (LazySingleton.class) {
+//                if (INSTANCE == null) {
+//                    INSTANCE = new LazySingleton();
+//                }
+//            }
+//        }
+//        return INSTANCE;
+    }
+
+    private static class LazySingletonHolder {
+        private static final LazySingleton INSTANCE = new LazySingleton();
+    }
+}
