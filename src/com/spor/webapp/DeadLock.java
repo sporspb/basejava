38b18bc7594aba93bab2d3d3da964610b85ab27a
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-05-15 06:08:25 +0300

    HW11 update DeadLock

--- src/com/spor/webapp/DeadLock.java
+++ src/com/spor/webapp/DeadLock.java
@@ -4,56 +4,30 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class DeadLock {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Lock lock1 = new ReentrantLock();
         Lock lock2 = new ReentrantLock();
+        deadlock(lock1, lock2);
+        deadlock(lock2, lock1);
+    }
 
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("thread1 working");
-                lock1.lock();
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("thread1 waiting");
-                lock2.lock();
-                try {
-                    System.out.println("doSmth");
-                } finally {
-                    lock1.unlock();
-                    lock2.unlock();
-                }
+    private static void deadlock(Lock lock1, Lock lock2) {
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " working");
+            lock1.lock();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("thread2 working");
-                lock2.lock();
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("thread2 waiting");
-                lock1.lock();
-                try {
-                    System.out.println("doSmth");
-                } finally {
-                    lock2.unlock();
-                    lock1.unlock();
-                }
+            System.out.println(Thread.currentThread().getName() + " waiting");
+            lock2.lock();
+            try {
+                System.out.println("doSmth");
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
             }
-        });
-
-        thread1.start();
-        thread2.start();
-
-        thread1.join();
-        thread2.join();
-        System.out.println("Finished");
+        }).start();
     }
 }
