78073437948e4269a5c162060e091ab1d18fa089
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-05-19 21:41:39 +0300

    HW13 update SqlHelper.java, SqlStorage.java
    SqlHelper.java
    -> add constructor
    -> remove method getConnection
    -> remove interface executor
    -> remove method execute
    -> add getSQLState checking in method executeQuery
    
    refactor SqlStorage.java
    -> add field sqlHelper

--- src/com/spor/webapp/sql/SqlHelper.java
+++ src/com/spor/webapp/sql/SqlHelper.java
@@ -2,38 +2,26 @@ package com.spor.webapp.sql;
 
 import com.spor.webapp.exception.ExistStorageException;
 import com.spor.webapp.exception.StorageException;
-import org.postgresql.util.PSQLException;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class SqlHelper {
-    private static ConnectionFactory connectionFactory;
+    private ConnectionFactory connectionFactory;
 
-    public static void getConnection(String dbUrl, String dbUser, String dbPassword) {
-        connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+    public SqlHelper(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
     }
 
-    public static void execute(String command, executor executor) {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(command)) {
-            executor.doExecute(ps);
-        } catch (SQLException e) {
-            if (e instanceof PSQLException) {
-                System.out.println(e.getMessage());
-                throw new ExistStorageException(e);
-            }
-            throw new StorageException(e);
-        }
-    }
-
-    public static <T> T executeQuery(String query, queryExecutor<T> executor) {
+    public <T> T executeQuery(String query, queryExecutor<T> executor) {
         try (Connection conn = connectionFactory.getConnection();
              PreparedStatement ps = conn.prepareStatement(query)) {
             return executor.doExecuteQuery(ps);
         } catch (SQLException e) {
+            if (e.getSQLState().startsWith("23505")) {
+                throw new ExistStorageException(e);
+            }
             throw new StorageException(e);
         }
     }
@@ -41,10 +29,5 @@ public class SqlHelper {
     @FunctionalInterface
     public interface queryExecutor<T> {
         T doExecuteQuery(PreparedStatement preparedStatement) throws SQLException;
-    }
-
-    @FunctionalInterface
-    public interface executor {
-        void doExecute(PreparedStatement preparedStatement) throws SQLException;
     }
 }
--- src/com/spor/webapp/storage/SqlStorage.java
+++ src/com/spor/webapp/storage/SqlStorage.java
@@ -12,23 +12,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import static java.sql.DriverManager.getConnection;
+
 public class SqlStorage implements Storage {
     private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+    private SqlHelper sqlHelper;
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
-        SqlHelper.getConnection(dbUrl, dbUser, dbPassword);
+        sqlHelper = new SqlHelper(() -> getConnection(dbUrl, dbUser, dbPassword));
     }
 
     @Override
     public void clear() {
         LOG.info("Clear storage");
-        SqlHelper.execute("DELETE FROM resume", PreparedStatement::execute);
+        sqlHelper.executeQuery("DELETE FROM resume", PreparedStatement::execute);
     }
 
     @Override
     public Resume get(String uuid) {
         LOG.info("Get " + uuid);
-        return SqlHelper.executeQuery("SELECT * FROM resume r WHERE r.uuid =?", preparedStatement -> {
+        return sqlHelper.executeQuery("SELECT * FROM resume r WHERE r.uuid =?", preparedStatement -> {
             preparedStatement.setString(1, uuid);
             ResultSet rs = preparedStatement.executeQuery();
             if (!rs.next()) {
@@ -41,29 +44,32 @@ public class SqlStorage implements Storage {
     @Override
     public void update(Resume r) {
         LOG.info("Update " + r.getUuid());
-        SqlHelper.execute("UPDATE resume SET full_name = ? WHERE uuid = ?", preparedStatement -> {
+        sqlHelper.executeQuery("UPDATE resume SET full_name = ? WHERE uuid = ?", preparedStatement -> {
             preparedStatement.setString(1, r.getFullName());
             preparedStatement.setString(2, r.getUuid());
             checkNotExist(preparedStatement, r.getUuid());
+            return null;
         });
     }
 
     @Override
     public void save(Resume r) {
         LOG.info("Save " + r.getUuid());
-        SqlHelper.execute("INSERT INTO resume (uuid, full_name) VALUES (?,?)", preparedStatement -> {
+        sqlHelper.executeQuery("INSERT INTO resume (uuid, full_name) VALUES (?,?)", preparedStatement -> {
             preparedStatement.setString(1, r.getUuid());
             preparedStatement.setString(2, r.getFullName());
             preparedStatement.execute();
+            return null;
         });
     }
 
     @Override
     public void delete(String uuid) {
         LOG.info("Delete " + uuid);
-        SqlHelper.execute("DELETE FROM resume WHERE uuid = ?", preparedStatement -> {
+        sqlHelper.executeQuery("DELETE FROM resume WHERE uuid = ?", preparedStatement -> {
             preparedStatement.setString(1, uuid);
             checkNotExist(preparedStatement, uuid);
+            return null;
         });
     }
 
@@ -71,19 +77,19 @@ public class SqlStorage implements Storage {
     public List<Resume> getAllSorted() {
         LOG.info("GetAll");
         List<Resume> list = new ArrayList<>();
-        SqlHelper.execute("SELECT * FROM resume ORDER BY uuid, full_name", preparedStatement -> {
+        return sqlHelper.executeQuery("SELECT * FROM resume ORDER BY uuid, full_name", preparedStatement -> {
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
                 list.add(new Resume(rs.getString("uuid").trim(), rs.getString("full_name")));
             }
+            return list;
         });
-        return list;
     }
 
     @Override
     public int size() {
         LOG.info("Get storage size");
-        return SqlHelper.executeQuery("SELECT COUNT (*) FROM resume", preparedStatement -> {
+        return sqlHelper.executeQuery("SELECT COUNT (*) FROM resume", preparedStatement -> {
             ResultSet rs = preparedStatement.executeQuery();
             if (!rs.next()) {
                 throw new StorageException("Count error");
