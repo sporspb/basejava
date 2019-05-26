b29b9e67f5725b19aec5757f74fd8a58d78fd9e6
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-05-26 18:51:50 +0300

    HW15 add HTML table
    Config.java
    -> add absolute path
    
    ResumeServlet.java
    add HTML table
    add postgresql driver

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,22 +1,69 @@
 package ru.javawebinar.basejava.web;
 
+import com.spor.webapp.Config;
+import com.spor.webapp.model.Resume;
+import com.spor.webapp.storage.SqlStorage;
+import com.spor.webapp.storage.Storage;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class ResumeServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private Storage storage = new SqlStorage(com.spor.webapp.Config.get().getUrl(), com.spor.webapp.Config.get().getLogin(), Config.get().getPassword());
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-//        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        /*
         response.setContentType("text/html; charset=UTF-8");
         String name = request.getParameter("name");
         response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
-    }
+        */
+        PrintWriter out = response.getWriter();
 
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
+
+        try {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">");
+            out.println("<link rel=\"stylesheet\" href=\"css/style.css\">");
+            out.println("<title>Resumes Table</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h3>Resumes Table</h3>");
+            out.println("</body>");
+
+            List<Resume> list = storage.getAllSorted();
+
+            out.println("<table border=\"1\">");
+            out.println("<tr>");
+            out.println("<td>uuid</td>");
+            out.println("<td>fullname");
+            out.println("</tr>");
+            for (Resume resume : list) {
+                out.println("<tr>");
+                out.println("<td>" + resume.getUuid() + "</td>");
+                out.println("<td>" + resume.getFullName());
+                out.println("</tr>");
+            }
+            out.println("</table>");
+            out.println("</html>");
+        } catch (Exception e) {
+            out.println(e.getMessage());
+        }
+    }
 }
