8f86d1b5a59043d0d8995fcd68d85558f7bc1659
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-05-28 23:38:42 +0300

    HW16 add list.jsp
    add list.jsp
    update ResumeServlet.java

--- src/com/spor/webapp/web/ResumeServlet.java
+++ src/com/spor/webapp/web/ResumeServlet.java
@@ -1,8 +1,6 @@
 package com.spor.webapp.web;
 
 import com.spor.webapp.Config;
-import com.spor.webapp.model.ContactType;
-import com.spor.webapp.model.Resume;
 import com.spor.webapp.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -10,7 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class ResumeServlet extends javax.servlet.http.HttpServlet {
     private Storage storage; // = Config.get().getStorage();
@@ -25,42 +22,8 @@ public class ResumeServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
+        request.setAttribute("resumes", storage.getAllSorted());
         response.setCharacterEncoding("UTF-8");
-        response.setHeader("Content-Type", "text/html; charset=UTF-8");
-        /*
-        response.setContentType("text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
-        */
-        PrintWriter out = response.getWriter();
-
-        try {
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">");
-            out.println("<link rel=\"stylesheet\" href=\"css/style.css\">");
-            out.println("<title>Resumes Table</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h3>Resumes Table</h3>");
-            out.println("</body>");
-            out.println("<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">");
-            out.println("<tr>");
-            out.println("<th>Имя</th>");
-            out.println("<th>email</th>");
-            out.println("</tr>");
-            for (Resume resume : storage.getAllSorted()) {
-                out.println("<tr>");
-                out.println("<td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>");
-                out.println("<td>" + resume.getContact(ContactType.MAIL).getName() + "</td>");
-                out.println("</tr>");
-            }
-            out.println("</table>");
-            out.println("</html>");
-        } catch (Exception e) {
-            out.println(e.getMessage());
-        }
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
     }
 }
